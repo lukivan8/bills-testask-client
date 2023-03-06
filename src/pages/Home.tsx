cc739dcf64f814e6b57c9da8ae1fe7c10d09b7eb
CLII412 <sw412fr@gmail.com>
2023-03-06 12:24:44 +0200

    upd style import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonRouterLink } from "@ionic/react";
 import { TextElem } from "../common/text-elem";
 import { ImageElem } from "../common/image-elem";
-import NoteIcon from "../assets/svg/note-text.svg";
+import NoteIcon from "../assets/svg/note-icon.svg";
 import { Toolbar } from "../common/layout-toolbar";
 import { ItemWrapper } from "../common/item-wrapper";
 import { Card } from "../common/card";
@@ -20,6 +20,7 @@ export interface BillData {
   paymentAmount: number;
   creationTime: string;
 }
+``;
 
 const Home = () => {
   const { data, isLoading } = useQuery("bills", async () => {
@@ -56,13 +57,13 @@ const Home = () => {
             data.map((bill: BillData) => {
               return (
                 <Card key={bill.billId}>
-                  <Stack alignItems="flex-start" justifyContent="unset">
+                  <Stack alignItems="flexStart" justifyContent="unset">
                     <TextElem
                       size="label"
                       tid={"BILL.SERVICE_LABEL." + bill.transactionType}
                     />
                   </Stack>
-                  <Stack alignItems="flex-end" justifyContent="space-between">
+                  <Stack alignItems="flexEnd" justifyContent="spaceBetween">
                     <TextElem color="secondary" size="small">
                       <Moment format="YYYY/MM/DD HH:mm">
                         {bill.creationTime}
