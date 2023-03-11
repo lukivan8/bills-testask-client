871fac824cfd5403d83d278722a320437b941877
lukivan8 <lukivan888@gmail.com>
2023-03-11 22:44:53 +0600

    extracted status color to theme

--- src/epic/bill-create-form/frame/status.tsx
+++ src/epic/bill-create-form/frame/status.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import styled, {css} from "styled-components";
-import {FONT_SIZE_DATA, FONT_SIZE_ENUM} from "../../../theme/sizes";
+import styled, { css } from "styled-components";
+import { FONT_SIZE_DATA, FONT_SIZE_ENUM } from "../../../theme/sizes";
+import { STATUS_COLOR_DATA } from "../../../theme/color";
 
-const Status: React.FC<{ message?: string }> = ({message}) => {
-  return <React.Fragment>{message && <StyledContainer message={message}>{message}</StyledContainer>}</React.Fragment>
+const Status: React.FC<{ message?: string }> = ({ message }) => {
+  return (
+    <React.Fragment>
+      {message && (
+        <StyledContainer message={message}>{message}</StyledContainer>
+      )}
+    </React.Fragment>
+  );
 };
 
 const StyledContainer = styled.div<{ message: string }>`
@@ -14,8 +21,8 @@ const StyledContainer = styled.div<{ message: string }>`
   padding: 10px ;
   ${({message}) => {
     return css`
-      border-color: ${message === "Successful!" ? "#0da11c" : "#c9000a"};
-      color: ${message === "Successful!" ? "#0da11c" : "#c9000a"};
+      border-color: ${message === "Successful!" ? STATUS_COLOR_DATA["success"] : STATUS_COLOR_DATA["error"]};
+      color: ${message === "Successful!" ? STATUS_COLOR_DATA["success"] : STATUS_COLOR_DATA["error"]};
     `;
   }}
 `;
--- src/theme/color.tsx
+++ src/theme/color.tsx
@@ -14,3 +14,14 @@ export const TEXT_COLOR_DATA = {
   [TEXT_COLOR_ENUM.BUTTON_SOLID]: "#FFFFFF",
 };
 
+export enum STATUS_COLOR_ENUM {
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
+export type STATUS_COLOR_TYPE = `${STATUS_COLOR_ENUM}`;
+
+export const STATUS_COLOR_DATA = {
+  [STATUS_COLOR_ENUM.SUCCESS]: "#0da11c",
+  [STATUS_COLOR_ENUM.ERROR]:"#c9000a"
+};
